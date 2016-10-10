d23ea428e151ff124b080e5d090cb18849b7ccc4
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-10 13:36:21 -0700

    root file working

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render("index");
+});
+app.post('/users', function(req, res){
+  res.redirect('/');
+})
+
+app.listen(8000, function(){
+  console.log("listening on port 8000");
+});
